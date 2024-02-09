6696ff1e02e303c9bf42a5e10b37a15a228e8b00
Bálint Kristóf Fazekas <fbk.fazekas@gmail.com>
2024-02-09 08:37:51 +0100

    sikeres jegy kiválasztás

--- Duna Mozi/Frontend/JS/szekbetolt.js
+++ Duna Mozi/Frontend/JS/szekbetolt.js
@@ -1,4 +1,6 @@
 function fillSzekList(){
+    const selectedSeats = []
+
     const data = {
         method: "POST",
         headers: {"Content-Type" : "application/json",},
@@ -7,107 +9,114 @@ function fillSzekList(){
         })       
     }
 
-    fetch('http://localhost:8000/szekek',data)
-    .then((response) => {
-        return response.json();
-    }).then(data => {
-        document.getElementById("seatmapA").innerHTML = "";
-        document.getElementById("seatmapB").innerHTML = "";
-        document.getElementById("seatmapC").innerHTML = "";
-        document.getElementById("seatmapD").innerHTML = "";
-        document.getElementById("seatmapE").innerHTML = "";
-        document.getElementById("seatmapF").innerHTML = "";
-        const szekekButtonsA = document.getElementById("seatmapA");
-        const szekekButtonsB = document.getElementById("seatmapB");
-        const szekekButtonsC = document.getElementById("seatmapC");
-        const szekekButtonsD = document.getElementById("seatmapD");
-        const szekekButtonsE = document.getElementById("seatmapE");
-        const szekekButtonsF = document.getElementById("seatmapF");
+    fetch('http://localhost:8000/szekek', data)
+        .then((response) => {
+            return response.json();
+        })
+        .then(data => {
+            // Kitöröljük at előző adatokat
+            document.getElementById("seatmapA").innerHTML = "";
+            document.getElementById("seatmapB").innerHTML = "";
+            document.getElementById("seatmapC").innerHTML = "";
+            document.getElementById("seatmapD").innerHTML = "";
+            document.getElementById("seatmapE").innerHTML = "";
+            document.getElementById("seatmapF").innerHTML = "";
 
-        data.forEach((szekek) => {
-            if(szekek.sor == "A"){
-                if(szekek.foglalt == "1"){
-                    const div = document.createElement("div");
-                    div.id = "sor";                    
-                    div.innerHTML="<div class='seat' value="+szekek.sor +szekek.szekszam+" datasor="+szekek.sor +" "+"dataszam="+szekek.szekszam+" id='szek' style='background-color: red'>"+szekek.szekszam+"</div>"
-                    szekekButtonsA.appendChild(div);
-                } else {
-                    const div = document.createElement("div");
-                    div.id = "sor";                    
-                    div.innerHTML="<button value="+szekek.sor +szekek.szekszam+" datasor="+szekek.sor +" "+"dataszam="+szekek.szekszam+" id='szek' class='gomb'>"+szekek.szekszam+"</button>"
-                    szekekButtonsA.appendChild(div);
-                }
-                
+            // Szerezzen hivatkozásokat konténer elemekre
+            const szekekButtonsA = document.getElementById("seatmapA");
+            const szekekButtonsB = document.getElementById("seatmapB");
+            const szekekButtonsC = document.getElementById("seatmapC");
+            const szekekButtonsD = document.getElementById("seatmapD");
+            const szekekButtonsE = document.getElementById("seatmapE");
+            const szekekButtonsF = document.getElementById("seatmapF");
+
+            function updateSelectedSeats() {
+                const selectedSeatsSpan = document.getElementById("selected-seats");
+                selectedSeatsSpan.textContent = selectedSeats.join(', ');
             }
-            if(szekek.sor == "B"){
-                if(szekek.foglalt == "1"){
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}" style=background-color:red>${szekek.szekszam}</div>`
-                    szekekButtonsB.appendChild(div);
+
+            // ismételt végrehajtás az adatokon, és hozzon létre gombokat dinamikusan
+            data.forEach((szekek) => {
+                if (szekek.foglalt == "1") {
+                    const seatDiv = document.createElement("div");
+                    seatDiv.className = "seat";
+                    seatDiv.textContent = szekek.szekszam;
+                    seatDiv.style.backgroundColor = "red";
+
+                    // Adja hozzá a div-t a megfelelő tárolóhoz a sor alapján
+                    switch (szekek.sor) {
+                        case "A":
+                            szekekButtonsA.appendChild(seatDiv);
+                            break;
+                        case "B":
+                            szekekButtonsB.appendChild(seatDiv);
+                            break;
+                        case "C":
+                            szekekButtonsC.appendChild(seatDiv);
+                            break;
+                        case "D":
+                            szekekButtonsD.appendChild(seatDiv);
+                            break;
+                        case "E":
+                            szekekButtonsE.appendChild(seatDiv);
+                            break;
+                        case "F":
+                            szekekButtonsF.appendChild(seatDiv);
+                            break;
+                        default:
+                            break;
+                    }
                 } else {
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}">${szekek.szekszam}</div>`
-                    szekekButtonsB.appendChild(div);
+                    const button = document.createElement("button");
+                    button.value = szekek.ules_id;
+                    button.classList.add('gomb');
+                    button.setAttribute('id',szekek.ules_id);
+                    button.textContent = szekek.szekszam;
+
+                    // Adja hozzá az eseményfigyelőt a gombhoz
+                    button.addEventListener('click', function () {
+
+                        const seatIndex = selectedSeats.indexOf(szekek.szekszam + szekek.sor);
+                        if (seatIndex === -1) {
+
+                            selectedSeats.push(szekek.szekszam + szekek.sor);
+
+                        } else {
+
+                            selectedSeats.splice(seatIndex, 1);
+
+                        }
+
+                        updateSelectedSeats();
+                    });
+
+                    // A sor alapján fűzze hozzá a gombot a megfelelő tárolóhoz
+                    switch (szekek.sor) {
+                        case "A":
+                            szekekButtonsA.appendChild(button);
+                            break;
+                        case "B":
+                            szekekButtonsB.appendChild(button);
+                            break;
+                        case "C":
+                            szekekButtonsC.appendChild(button);
+                            break;
+                        case "D":
+                            szekekButtonsD.appendChild(button);
+                            break;
+                        case "E":
+                            szekekButtonsE.appendChild(button);
+                            break;
+                        case "F":
+                            szekekButtonsF.appendChild(button);
+                            break;
+                        default:
+                            break;
+                    }
                 }
-            }
-            if(szekek.sor == "C"){
-                if(szekek.foglalt == "1"){
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}"data-seat="${szekek.sor}${szekek.szekszam}" style=background-color:red>${szekek.szekszam}</div>`
-                    szekekButtonsC.appendChild(div);
-                } else {
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}">${szekek.szekszam}</div>`
-                    szekekButtonsC.appendChild(div);
-                }
-            }
-            if(szekek.sor == "D"){
-                if(szekek.foglalt == "1"){
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}" style=background-color:red>${szekek.szekszam}</div>`
-                    szekekButtonsD.appendChild(div);
-                } else {
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}">${szekek.szekszam}</div>`
-                    szekekButtonsD.appendChild(div);
-                }
-            }
-            if(szekek.sor == "E"){
-                if(szekek.foglalt == "1"){
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}" style=background-color:red>${szekek.szekszam}</div>`
-                    szekekButtonsE.appendChild(div);
-                } else {
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}">${szekek.szekszam}</div>`
-                    szekekButtonsE.appendChild(div);
-                }
-            }
-            if(szekek.sor == "F"){
-                if(szekek.foglalt == "1"){
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}" style=background-color:red>${szekek.szekszam}</div>`
-                    szekekButtonsF.appendChild(div);
-                } else {
-                    const div = document.createElement("div");
-                    div.className = "ulessor"
-                    div.innerHTML=`<div class='seat' id="${szekek.sor}${szekek.szekszam}" data-seat="${szekek.sor}${szekek.szekszam}">${szekek.szekszam}</div>`
-                    szekekButtonsF.appendChild(div);
-                }
-            }
+            });
+        })
+        .catch((error) => {
+            console.log(error);
         });
-    })
-
-    .catch((error) => {
-        console.log(error);
-    });
 }
